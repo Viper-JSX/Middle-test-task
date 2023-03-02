bd85c0e0381d6ef259d1af0072ecd7f916a2daa0
Yurii Shtefanko <yuramcpecs@gmail.com>
2023-03-02 22:35:12 +0200

    Implemented FolderItem

--- src/components/Desktop_window/Desktop_window.js
+++ src/components/Desktop_window/Desktop_window.js
@@ -1,8 +1,15 @@
+import { useSelector } from "react-redux";
 import { Outlet } from "react-router";
+import FolderItem from "../Items/Folder_item";
 
 function DesktopWindow () {
+    const folders = useSelector((state) => Object.values(state.folders.folders));
+
     return(
         <div className="desktopWindow">
+            {
+                folders.map((folder) => <FolderItem folder={folder} key={`${folder.name}_folder`} />)
+            }
             <Outlet />
         </div>
     );
--- src/components/Items/Folder_item.js
+++ src/components/Items/Folder_item.js
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 function FolderItem ({ folder }) {
     return(
-        <Link>{ folder.name }</Link>
+        <Link to={`folders/${folder.name.toLowerCase()}`}>{ folder.name }</Link>
     );
 }
 
