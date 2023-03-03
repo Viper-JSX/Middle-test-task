21a4867f0fe05b817d32692a0da6ce86c42c2e39
Yurii Shtefanko <yuramcpecs@gmail.com>
2023-03-03 11:13:04 +0200

    Added title to FileItem

--- src/components/Items/File_item.js
+++ src/components/Items/File_item.js
@@ -1,8 +1,16 @@
+import bytes from "bytes";
 import { Link } from "react-router-dom";
 import FileIcon from "./File_icon";
 
 function FileItem ({ file }) {
-    const itemTitle = `Created: ${ file.atime }\nSize: ${ file.size }\nModified: ${ file.mtime }
+    const createdDate =  new Date(file.atime);
+    const modifiedDate = new Date(file.mtime);
+    const createdDateString = `${ createdDate.toDateString() } - ${ String(createdDate.getHours()).padStart(2, "0") }:${ String(createdDate.getMinutes()).padStart(2, "0") }`;
+    const modifiedDateString = `${ modifiedDate.toDateString() } - ${ String(modifiedDate.getHours()).padStart(2, "0") }:${ String(modifiedDate.getMinutes()).padStart(2, "0")}`;
+
+    const size = bytes(file.size, { decimalPlaces: 0 });
+
+    const itemTitle = `Created: ${ createdDateString }\nSize: ${ size }\nModified: ${ modifiedDateString }
     `;
 
     return(
