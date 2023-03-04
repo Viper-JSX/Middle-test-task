0c9af748c84589f9d23ac7fd33e022d3c3eee85b
Yurii Shtefanko <yuramcpecs@gmail.com>
2023-03-04 08:58:49 +0200

    Added transitions, tiny color configurations

--- src/components/Folder/Folder_pannel/Folder_control.js
+++ src/components/Folder/Folder_pannel/Folder_control.js
@@ -1,10 +1,16 @@
 import { Link } from "react-router-dom";
+import { faXmark } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 function FolderControl () {
     return( 
         <nav className="folder-control">
             <ul>
-                <li className="close-button"><Link to="/">X</Link></li>
+                <li className="close-button">
+                    <Link to="/">
+                        <FontAwesomeIcon icon={ faXmark } />
+                    </Link>
+                </li>
             </ul>
         </nav>
     );
