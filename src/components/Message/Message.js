8eed62e2977b9faf9b7c6723db0a57bb8101fd9f
Yurii Shtefanko <yuramcpecs@gmail.com>
2023-03-04 11:36:33 +0200

    Created Message component

---
+++ src/components/Message/Message.js
@@ -0,0 +1,16 @@
+import { useSelector } from "react-redux";
+
+function Message () {
+    const message = useSelector((state) => state.message);
+    const title = message.title || "";
+    const text = message.text || "";
+
+    return(
+        <div className={ `message ${message ? "visible" : "hidden"}` }>
+            <b className="message__title">{ title }</b>
+            <p className="message__text">{ text }</p>
+        </div>
+    );
+}
+
+export default Message;
