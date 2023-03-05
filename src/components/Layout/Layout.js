09b7e45bda1199b906b2f86b97c028c9f666a129
Yurii Shtefanko <yuramcpecs@gmail.com>
2023-03-05 18:56:11 +0200

    Created loader component

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -3,6 +3,7 @@ import { Routes, Route } from "react-router";
 import Desktop from "../Desktop/Desktop";
 import DesktopWindow from "../Desktop_window/Desktop_window";
 import Folder from "../Folder/Folder";
+import Loader from "../Loader/Loader";
 import Message from "../Message/Message";
 
 function Layout (
@@ -23,6 +24,7 @@ function Layout (
                 </Routes>
 
                 <Message />
+                <Loader />
             </Desktop>
         </div>
     );
---
+++ src/components/Loader/Loader.js
@@ -0,0 +1,15 @@
+import { CircularProgress } from "@mui/material";
+
+function Loader ( loading ) {
+    if (!loading) {
+        return null;
+    }
+
+    return(
+        <div className="loader">
+            <CircularProgress />
+        </div>
+    );  
+}
+
+export default Loader;
