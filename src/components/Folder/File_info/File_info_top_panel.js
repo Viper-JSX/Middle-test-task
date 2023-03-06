43e67d27d03ce67eb2cc1238b7a447c724ff5c08
Yurii Shtefanko <yuramcpecs@gmail.com>
2023-03-06 13:05:46 +0200

    Added animation to info window

--- src/components/Folder/File_info/File_info_top_panel.js
+++ src/components/Folder/File_info/File_info_top_panel.js
@@ -3,6 +3,7 @@ import FileInfoControl from "./File_info_control";
 function FileInfoTopPanel ({ fileName, handleFileClose }) {
     return(
         <div className="file-info-top-panel">
+            <div></div>
             <b className="filename">{ fileName }</b>
             <FileInfoControl handleFileClose={handleFileClose} />
         </div>
