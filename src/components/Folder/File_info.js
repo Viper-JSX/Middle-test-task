2c9e1ef2be4fd27102d22ed95670f183b97967d2
Yurii Shtefanko <yuramcpecs@gmail.com>
2023-03-06 12:10:45 +0200

    Initialized opened file state

--- src/components/Folder/File_info.js
+++ src/components/Folder/File_info.js
@@ -1,12 +1,16 @@
+import Container from "../Container/Container";
+
 function FileInfo ({ info }) {
     return(
-        <div className="file-info">
-            <span><b>Size: </b>{ info.size }</span>
-            <span><b>Created: </b>{ info.createDate }</span>
-            <span><b>Size: </b>{ info.modifyDate }</span>
+        <Container>
+            <div className="file-info">
+                <span><b>Size: </b>{ info.size }</span>
+                <span><b>Created: </b>{ info.createDate }</span>
+                <span><b>Size: </b>{ info.modifyDate }</span>
             
-            <button>X</button>
-        </div>
+                <button>X</button>
+            </div>
+        </Container>
     );
 }
 
