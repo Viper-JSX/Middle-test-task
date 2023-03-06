b6fab348aab68d29445b215359b438e157128b7d
Yurii Shtefanko <yuramcpecs@gmail.com>
2023-03-06 12:07:42 +0200

    Created FileInfo component

---
+++ src/components/Folder/File_info.js
@@ -0,0 +1,13 @@
+function FileInfo ({ info }) {
+    return(
+        <div className="file-info">
+            <span><b>Size: </b>{ info.size }</span>
+            <span><b>Created: </b>{ info.createDate }</span>
+            <span><b>Size: </b>{ info.modifyDate }</span>
+            
+            <button>X</button>
+        </div>
+    );
+}
+
+export default FileInfo;
