89732f4e4f796fb19cb1ea0a9abafa7c212e8cb7
Yurii Shtefanko <yuramcpecs@gmail.com>
2023-03-06 14:04:48 +0200

    Removed unnecessary property

--- src/App.js
+++ src/App.js
@@ -2,15 +2,14 @@ import "./scss/main.scss";
 
 import { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import Layout from "./components/layout/Layout";
 import { changeFoldersSortCriteria } from "./redux/action_creators/folders_action_creators";
 import { getFolders } from "./redux/thunks/folders_thunks";
-import { showMessage } from "./redux/thunks/message_thunks";
+import Layout from "./components/layout/Layout";
 import readFromLocalStorage from "./utilities/read_from_local_storage";
+
 
 function App(){
     const dispatch = useDispatch();
-    const [ folders, message ] = useSelector((state) => [ state.folders, state.message ]);
     const [ appIsLoading, setAppIsLoading ] = useState(true);
 
     window.addEventListener("load", handleAppLoaded);
